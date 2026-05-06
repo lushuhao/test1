a768ff6d6b2c33057deb5a67e41242c4674613eb
burl.lu <burl.lu@temu.com>
2026-05-06 20:05:41 +0800

    feat: three modify

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ function App(): any {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/*<a href="https://react.dev" target="_blank">*/}
+        {/*  <img src={reactLogo} className="logo react" alt="React logo" />*/}
+        {/*</a>*/}
       </div>
       <h1>Vite + React console.log('test code 2')</h1>
       <div className="card">
