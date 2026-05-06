1e279089e6034d560e5159e05c4fcb9981774fbc
burl.lu <burl.lu@temu.com>
2026-05-06 20:01:47 +0800

    feat: first modify

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+    console.log('test code 1')
+
   return (
     <>
       <div>
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React console.log('test code 2')</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
